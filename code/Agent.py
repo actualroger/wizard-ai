Reverted a priority, with normalization. Fixed q priority without beta schedule. Added many runs.

code/Agent.py
@@ -181,7 +181,7 @@ class NNAgent(Agent):
         # apply weighting if necessary
         match self.params['buffer_type']:
             case 'action_priority':
-                loss = (torch.tensor(self.buffer.prev_weights) * baseLoss).mean()
+                loss = (baseLoss / torch.tensor(self.buffer.prev_weights)).mean()
             case 'q_priority':
                 errors = torch.abs(q_behavior - q_target)
                 self.buffer.setErrors(self.buffer.prev_indices, errors)

code/Buffer.py
@@ -118,8 +118,8 @@ class PriorityBuffer(ReplayBuffer):
                                   p=normWeights,
                                   replace=False,
                                   shuffle=False)
-        is_weight = [normWeights[i] for i in indices]
-        self.prev_weights = is_weight / is_weight.max()
+        is_weight = np.array([normWeights[i] for i in indices])
+        self.prev_weights = is_weight / is_weight.min()
         return self._encode_sample(indices)
 
     def priorityFunction(self, transition) -> float:
@@ -213,7 +213,7 @@ class QPriorityBuffer(PriorityBuffer):
         if self.betaSchedule is not None:
             is_weight = np.power(self.__len__() * rawWeights, -self.betaSchedule.getValue(self.step))
         else:
-            is_weight = np.array(self.__len__() * rawWeights)
+            is_weight = np.array(1 / (self.__len__() * rawWeights))
         self.step += 1
         self.prev_weights = is_weight / is_weight.max()
         self.prev_indices = indices